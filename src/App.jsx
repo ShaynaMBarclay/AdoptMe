8e814cbf5464997b14ec6a1c06a73a6113fc8722
Kateryna Salata <katerynasalata@icloud.com>
2024-11-26 20:02:02 +0100

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import "./App.css";
 import dataAnimals from "./assets/data.json";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import AboutPage from "./Pages/AboutPage";
-import HomePage from "./Pages/HomePage";
-import DetailsPage from "./Pages/DetailsPage";
-import NotFoundPage from "./Pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/details/:cardId" element={<DetailsPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details/:cardId" element={<DetailsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       <Footer />
-    </Routes>
+    </div>
   );
 }
 
