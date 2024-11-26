866b35bfb544cf2cfabbd71ed1022f11f96c48b9
Kateryna Salata <katerynasalata@icloud.com>
2024-11-26 19:31:16 +0100

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/ShaynaMBarclay/AdoptMe.git">GitHub Repo</a>
+      <a href="#">Find a friend</a>
+      <a href="#">About us</a>
+      <a href="#">Contacts</a>
+    </div>
+  );
+};
+
+export default Footer;
