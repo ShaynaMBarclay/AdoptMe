be2400f6ae6ebe72820ef5a85a5646b72fdbaf9e
ShaynaMBarclay <shaynambarclay@gmail.com>
2024-11-26 20:29:53 +0100

    navbar buttons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>AdoptMe</h1>
+      <div className="navbar-links">
+        <Link to="/about" className="navbar-link">About</Link>
+        <Link to="/details/:cardId" className="navbar-link">Detail</Link>
+        <Link to="/" className="navbar-link">Home</Link>
+      </div>
     </nav>
   );
 };
