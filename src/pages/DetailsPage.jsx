453c2a83c3318126f6c6fdf4c7d8f39799eb9125
Kateryna Salata <katerynasalata@icloud.com>
2024-12-02 22:16:05 +0100

    add picture size, delete obvious comments

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,12 +1,7 @@
-// Comments added by Shayna to explain her work and what each piece of code does
-
-// Import necessary modules and hooks from React and React Router
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import SubmitForm from "../components/SubmitForm"; // Import SubmitForm
-const test =
-  "https://res.cloudinary.com/dmvawq2ak/image/upload/v1732977208/simona1_meam0a.jpg";
-// Define the DetailsPage functional component
+import SubmitForm from "../components/SubmitForm";
+
 function DetailsPage() {
   // state to control the modal visibility
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,64 +14,41 @@ function DetailsPage() {
   // Destructure the `animal` object from the location's state or set it to undefined if not present
   const { animal } = location.state || {};
 
-  // Check if no animal data was passed to this page
   if (!animal) {
     return (
       <div>
-        {/* Display a message if no animal data is available */}
         <p>No animal data available.</p>
-
-        {/* Button to navigate back to the homepage */}
-
         <button onClick={() => navigate("/")}>Go Back to Homepage</button>
       </div>
     );
   }
 
-  // Function to open the modal
   const openModal = () => {
     setIsModalOpen(true);
   };
-
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  // Render the details of the animal if data is available
   return (
     <div>
-      {/* Display the animal's name */}
       <h1>{animal.name}</h1>
-      {/* 
       <img
-        src={animal.image}
+        src={animal.images[0]}
         alt={animal.name}
-        style={{ width: "300px", height: "auto" }}
-      /> */}
-
-      {/* Display the animal's image with a fixed width */}
-      <img
-        src={test}
-        alt={animal.name}
-        style={{ width: "300px", height: "auto" }}
+        style={{ height: "500px" }}
       />
-
-      {/* Display the animal's species */}
       <p>
         <strong>Species:</strong> {animal.species}
       </p>
-
-      {/* Display the animal's age */}
       <p>
         <strong>Age:</strong> {animal.age}
       </p>
 
-      {/* Display a description of the animal */}
       <p>{animal.description}</p>
 
-      {/* Button to open the modal */}
       <button className="details-btn" onClick={openModal}>
-        Adopt Me 🦝
+        Adopt {animal.name} 🦝
       </button>
 
       {/* Modal Logic */}
@@ -107,6 +79,4 @@ function DetailsPage() {
 }
 */
 }
-
-//Export the DetailsPage component so it can be imported and used in other files
 export default DetailsPage;
