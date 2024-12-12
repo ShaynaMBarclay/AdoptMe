4252ad77a177bdbb51a95209a60a09c52c4b8061
Kateryna Salata <katerynasalata@icloud.com>
2024-12-12 20:25:38 +0100

    fix merge

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,57 +2,47 @@ import { Link } from "react-router-dom";
 const AboutPage = () => {
   return (
     <>
-      <h1>About us</h1>
+      <h1>What is Adopt Me and what do we do?</h1>
       <div className="p1">
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733423691/bve084bq0djpdr9laled.png"
           alt="About Page"
         />
         <p>
-          AdoptMe is a project aimed at systematically solving the problem of
-          homeless animals in humane ways. Thanks to the Free Online resource,
-          you can find a pet or help a homeless animal find its family.
+          Adopt Me is a non-profit project aimed at systematically solving the
+          problem of homeless animals in humane ways. Thanks to the Free Online
+          resource, you can find a pet or help a homeless animal find its
+          family.
         </p>
       </div>
-      <h2>How does it work?</h2>
+      <h1>How it's work?</h1>
       <div className="about">
-        <div className="about about-text-column">
-          <h3>Adopting an animal</h3>
-          <ol>
-            <li>
-              Explore: browse the website to view animals with details like age,
-              location, and health status.
-            </li>
-            <li>
-              Search: use filters to find the perfect match based on your
-              preferences.
-            </li>
-            <li>
-              Submit a form: once you find an animal you are interested in, fill
-              out the an adoption inquiry form.{" "}
-            </li>
-            <li>
-              Wait for a Response: the shelter will contact you within two weeks
-              to discuss the next steps.
-            </li>
-          </ol>
-          <h3>Help an animal to find new home</h3>
-          <p>
-            Simply go to the{" "}
-            <Link to="/add-animal" className="add-animal-page">
-              Add Animal Page
-            </Link>
-            <br />
-            section from the homepage and fill in all the required information
-            about the pet.
-          </p>
-        </div>
+        <p>
+          This is a project that allows anyone to help our smaller brothers.
+          <br />
+          How? <br />
+          On the homepage, you can choose any pet that you like. Then, by
+          clicking the "Adopt" button, you fill in some basic information about
+          yourself.
+          <br /> After that, we will contact you for further arrangements.
+          <br />
+          Also, if you find an animal on the street and are unable to keep it at
+          home, but would like to help find it a family, you can use our
+          website.
+          <br /> Simply go to the{" "}
+          <Link to="/add-animal" className="add-animal-page">
+            Add Animal Page
+          </Link>
+          <br />
+          section from the homepage and fill in all the required information
+          about the pet.
+        </p>
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733177827/simona1_blfyoz.jpg"
           alt="About Page"
         />
       </div>
-      <h2>Our mission</h2>
+      <h1>Our mission</h1>
       <div className="mission">
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733255658/Lypa1_c1vr2u.jpg"
@@ -63,8 +53,7 @@ const AboutPage = () => {
           life of animals.
         </p>
       </div>
-
-      <h2>Our goal</h2>
+      <h1>Our goal</h1>
       <div className="goal">
         <p>Every animal has a loving owner.</p>
         <img
@@ -72,6 +61,7 @@ const AboutPage = () => {
           alt="About Page"
         />
       </div>
+      <h1>Thanks for helping🙏</h1>
 
       <h2>CONTACT US</h2>
       <p>
@@ -80,12 +70,13 @@ const AboutPage = () => {
         🕻 00000000 <br />
         Ask the chatbot on the Home Page! <br />
       </p>
-      <div>
-        <h2>People who create the web page for this project:</h2>
-        <p>Kateryna Salata</p>
-        <p>Shayna Barclay</p>
-        <p>Anna Turek</p>
-      </div>
+
+      <h2>People who create the web page for this project:</h2>
+      <br />
+
+      <p>KATYA SALATA</p>
+      <p>SHAYNA BARCLAY</p>
+      <p>ANNA TUREK</p>
     </>
   );
 };
