36a64161e7e8fc4503fc02b047c6e3c8ee6032be
Kateryna Salata <katerynasalata@icloud.com>
2024-12-13 02:42:47 +0100

    add css footer and about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,32 @@
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="footer">
-      <div>
-        <a
-          href="https://github.com/ShaynaMBarclay/AdoptMe.git"
-          className="footer-link"
-        >
-          GitHub Repo
-        </a>{" "}
-        <Link to="/" className="footer-link">
-          Find a friend
-        </Link>
-        <Link to="/about" className="footer-link">
-          About Us
-        </Link>
-      </div>
-      <span className="copyright">Copyright © 2024 Adopt Me</span>
-    </div>
+    <>
+      <footer className="footer">
+        <div className="footer-box">
+          <div className="footer-links">
+            <a
+              href="https://github.com/ShaynaMBarclay/AdoptMe.git"
+              className="footer-link"
+            >
+              GitHub Repo
+            </a>{" "}
+            <Link to="/" className="footer-link">
+              Find a friend
+            </Link>
+            <Link to="/about" className="footer-link">
+              About Us
+            </Link>
+          </div>
+          <section className="footer-contacts">
+            <h4>Contact us</h4>
+            <p>🖃 AdoptMe@FakeEmail.com</p>
+            <p>🕻 00000000 </p>
+          </section>
+        </div>
+        <span className="copyright">Copyright © 2024 AdoptMe</span>
+      </footer>
+    </>
   );
 };
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,83 +1,124 @@
 import { Link } from "react-router-dom";
 const AboutPage = () => {
+  const picAnna =
+    "https://res.cloudinary.com/dmvawq2ak/image/upload/v1734052410/1734037165872_l6khpz.jpg";
+  const picShayna =
+    "https://res.cloudinary.com/dmvawq2ak/image/upload/v1734052796/1728245495165_xysokp.jpg";
+  const picKatya =
+    "https://res.cloudinary.com/dmvawq2ak/image/upload/v1734053048/1728847868286_ezkj7a.jpg";
+  const imgGithub =
+    "https://res.cloudinary.com/dmvawq2ak/image/upload/v1734051523/25231_dedr2s.png";
+  const imgLinked =
+    "https://res.cloudinary.com/dmvawq2ak/image/upload/v1734051196/Linkedin_ymeqxm.png";
   return (
-    <>
-      <h1>What is Adopt Me and what do we do?</h1>
-      <div className="p1">
+    <div className="about-wrapper">
+      <h1>About us</h1>
+      <div className="about-box">
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733423691/bve084bq0djpdr9laled.png"
-          alt="About Page"
+          alt="dog Ragnar photo"
         />
-        <p>
-          Adopt Me is a non-profit project aimed at systematically solving the
-          problem of homeless animals in humane ways. Thanks to the Free Online
-          resource, you can find a pet or help a homeless animal find its
-          family.
+        <p className="about-text">
+          AdoptMe is a project aimed at systematically solving the problem of
+          homeless animals in humane ways. Thanks to the Free Online resource,
+          you can find a pet or help a homeless animal find its family.
         </p>
       </div>
-      <h1>How it's work?</h1>
-      <div className="about">
-        <p>
-          This is a project that allows anyone to help our smaller brothers.
-          <br />
-          How? <br />
-          On the homepage, you can choose any pet that you like. Then, by
-          clicking the "Adopt" button, you fill in some basic information about
-          yourself.
-          <br /> After that, we will contact you for further arrangements.
-          <br />
-          Also, if you find an animal on the street and are unable to keep it at
-          home, but would like to help find it a family, you can use our
-          website.
-          <br /> Simply go to the{" "}
-          <Link to="/add-animal" className="add-animal-page">
-            Add Animal Page
-          </Link>
-          <br />
-          section from the homepage and fill in all the required information
-          about the pet.
-        </p>
+      <h2>How does it work?</h2>
+      <div className="about-box">
+        <div className=" about-box-column">
+          <h3>Adopting an animal</h3>
+          <ol>
+            <li className="about-text">
+              Explore: browse the website to view animals with details like age,
+              location, and health status.
+            </li>
+            <li className="about-text">
+              Search: use filters to find the perfect match based on your
+              preferences.
+            </li>
+            <li className="about-text">
+              Submit a form: once you find an animal you are interested in, fill
+              out the an adoption inquiry form.{" "}
+            </li>
+            <li className="about-text">
+              Wait for a Response: the shelter will contact you within two weeks
+              to discuss the next steps.
+            </li>
+          </ol>
+          <h3>Help an animal to find new home</h3>
+          <p className="about-text">
+            Simply go to the{" "}
+            <Link className="about-link" to="/add-animal">
+              Add Animal Page
+            </Link>{" "}
+            and fill in all the required information about the pet.
+          </p>
+        </div>
         <img
+          className="about-pic-height"
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733177827/simona1_blfyoz.jpg"
-          alt="About Page"
+          alt="cat Simona picture"
         />
       </div>
-      <h1>Our mission</h1>
-      <div className="mission">
+      <h2>Our mission</h2>
+      <div className="about-box">
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733255658/Lypa1_c1vr2u.jpg"
-          alt="About Page"
+          alt="cat Lypa picture"
         />
-        <p>
+        <p className="about-text">
           Increase the level of responsibility and respect of society for the
           life of animals.
         </p>
       </div>
-      <h1>Our goal</h1>
-      <div className="goal">
-        <p>Every animal has a loving owner.</p>
+      <h2>Our goal</h2>
+      <div className="about-box">
+        <p className="about-text">Every animal has a loving owner.</p>
         <img
           src="https://res.cloudinary.com/dmvawq2ak/image/upload/v1733564468/sofi_pbwfh7.jpg"
-          alt="About Page"
+          alt="cat Sofi picture"
         />
       </div>
-      <h1>Thanks for helping🙏</h1>
-
-      <h2>CONTACT US</h2>
-      <p>
-        🖃 AdoptMe@FakeEmail.com
-        <br />
-        🕻 00000000 <br />
-        Ask the chatbot on the Home Page! <br />
-      </p>
-
-      <h2>People who create the web page for this project:</h2>
-      <br />
-
-      <p>KATYA SALATA</p>
-      <p>SHAYNA BARCLAY</p>
-      <p>ANNA TUREK</p>
-    </>
+      <section className="about-devs">
+        <h2>Development Team</h2>
+        <div className="dev-card-box">
+          <div className="dev-card">
+            <img className="dev-photo" src={picKatya} />
+            <h3>Kateryna Salata</h3>
+            <a href="https://www.linkedin.com/in/kateryna-salata-a54b111aa">
+              <img className="dev-card-linkedin" src={imgLinked} />
+            </a>
+            <a href="https://github.com/kateryna-salata">
+              {" "}
+              <img className="dev-card-github" src={imgGithub} />
+            </a>
+          </div>
+          <div className="dev-card">
+            <img className="dev-photo" src={picShayna} />
+            <h3>Shayna Barclay</h3>
+            <a href="https://www.linkedin.com/in/shaynambarclay/">
+              {" "}
+              <img className="dev-card-linkedin" src={imgLinked} />
+            </a>
+            <a href="https://github.com/ShaynaMBarclay">
+              {" "}
+              <img className="dev-card-github" src={imgGithub} />
+            </a>
+          </div>
+          <div className="dev-card">
+            <img className="dev-photo" src={picAnna} />
+            <h3>Anna Turek</h3>
+            <a href="https://www.linkedin.com/in/anna-turek-7694931a3/">
+              <img className="dev-card-linkedin" src={imgLinked} />
+            </a>
+            <a href="https://github.com/Unicorn3737">
+              <img className="dev-card-github" src={imgGithub} />
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
